b5421dd7fcd277a48acbeaca2f1620c086448cd8
tgishyan <tgishyan@dataart.com>
2023-04-27 17:37:33 +0400

    Fixes

--- Package.swift
+++ Package.swift
@@ -1,32 +1,40 @@
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version:5.5
+
 import PackageDescription
 
 let package = Package(
     name: "SugarKit",
-    platforms: [
-        .iOS(.v13)
-    ],
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SugarKit",
             targets: ["SugarKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Gishyan93/SugarToast", branch: "main"),
-        .package(url: "https://github.com/Gishyan93/SugarFAB", branch: "main")
+        .package(url: "https://github.com/myorg/SugarFAB.git", branch: "main"),
+        .package(url: "https://github.com/myorg/SugarToast.git", branch: "main")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "SugarKit", dependencies: ["SugarToast", "SugarFAB", "SugarKit"]),
-        .target(name: "SugarToast", dependencies: []),
-        .target(name: "SugarFAB", dependencies: []),
+        .target(
+            name: "SugarKit",
+            dependencies: [
+                .product(name: "SugarFAB", package: "SugarFAB"),
+                .product(name: "SugarToast", package: "SugarToast"),
+            ],
+            swiftSettings: [
+                .unsafeFlags([
+                    "-Xfrontend", "-disable-implicit-concurrency-module-import",
+                    "-Xfrontend", "-enable-experimental-concurrency"
+                ])
+            ],
+            linkerSettings: [
+                .linkedLibrary("SwiftConcurrency")
+            ]),
         .testTarget(
             name: "SugarKitTests",
-            dependencies: ["SugarKit"]
-        ),
+            dependencies: ["SugarKit"]),
     ]
 )
